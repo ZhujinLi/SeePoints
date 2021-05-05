6dc85b801d05806feee1d222f391f7fc349df04c
ZhuJin Li <lizhujin@outlook.com>
2021-05-06 01:22:41 +0800

    Support x- and y- selection

--- src/index.js
+++ src/index.js
@@ -14,49 +14,50 @@ Plotly.newPlot(
     { responsive: true, fillFrame: true },
 );
 
-let pastedText = "";
+let nums = [];
 
 document.onpaste = (e) => {
+    const pastedText = e.clipboardData.getData("Text");
+    nums = find_num(pastedText);
+    if (nums.length == 0 || nums[0].length == 0) {
+        alert("No valid number detected...");
+        return;
+    }
+
+    // First column is a special index
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = [i].concat(nums[i]);
+    }
+
     $("#config-modal").modal("show");
-    pastedText = e.clipboardData.getData("Text");
 
     const configBody = $("#config-modal-body");
     configBody.empty();
 
-    const nums = find_num(pastedText);
+    const nColumns = nums[0].length;
 
     const table = document.createElement("table");
     table.className = "table small table-borderless";
-    for (let i = 0; i < nums.length; i++) {
-        const row = table.insertRow();
-
-        if (i > 2) {
-            for (let j = 0; j < nums[i].length + 1; j++) {
-                row.insertCell().appendChild(document.createTextNode("..."));
-            }
-            break;
-        }
-
-        const id = document.createElement("b");
-        id.innerHTML = i;
-        row.insertCell().append(id);
-
-        for (let j = 0; j < nums[i].length; j++) {
-            row.insertCell().appendChild(document.createTextNode(nums[i][j]));
-        }
-    }
-
     configBody.append(table);
+
+    add_selector_row_to_table(table, nColumns, "x-selector-radio");
+    $("input[name=x-selector-radio][value=0]").prop("checked", true);
+
+    fill_table_with_numbers(table, nums);
+
+    add_selector_row_to_table(table, nColumns, "y-selector-radio");
+    $("input[name=y-selector-radio][value=0]").prop("checked", true);
 };
 
 document.getElementById("confirm-button").onclick = () => {
     $("#config-modal").modal("hide");
 
-    const nums = find_num(pastedText);
+    const xCol = $("input[name=x-selector-radio]:checked").val();
+    const yCol = $("input[name=y-selector-radio]:checked").val();
 
     const x = [], y = [];
-    nums.forEach(elem => x.push(elem[0]));
-    nums.forEach(elem => y.push(elem[1]));
+    nums.forEach(line => x.push(line[xCol]));
+    nums.forEach(line => y.push(line[yCol]));
 
     plotData.push({
         x: x,
@@ -69,3 +70,45 @@ document.getElementById("confirm-button").onclick = () => {
 
     Plotly.redraw("plot-div");
 };
+
+function add_selector_row_to_table(table, nColumns, name) {
+    const selectorRow = table.insertRow();
+    for (let i = 0; i < nColumns; i++) {
+        const colForm = document.createElement("div");
+        colForm.className = "form-check";
+
+        const colInput = document.createElement("input");
+        colInput.className = "form-check-input";
+        colInput.type = "radio";
+        colInput.name = name;
+        colInput.value = i;
+        colForm.appendChild(colInput);
+
+        selectorRow.insertCell().appendChild(colForm);
+    }
+}
+
+/**
+ * @param {HTMLTableElement} table 
+ * @param {number[][]} nums 
+ */
+function fill_table_with_numbers(table, nums) {
+    for (let i = 0; i < nums.length; i++) {
+        const row = table.insertRow();
+
+        if (i > 2) {
+            for (let j = 0; j < nums[i].length; j++) {
+                row.insertCell().appendChild(document.createTextNode("..."));
+            }
+            break;
+        }
+
+        const id = document.createElement("b");
+        id.innerHTML = nums[i][0];
+        row.insertCell().append(id);
+
+        for (let j = 1; j < nums[i].length; j++) {
+            row.insertCell().appendChild(document.createTextNode(nums[i][j]));
+        }
+    }
+}
